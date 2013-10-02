c956263cd13b301cf4c907f6da9864dd8746b787
Andr Ramos <andrerfsilva@gmail.com>
2013-10-02 19:55:39 -0300

    Minimax with alpha-beta pruning

--- models/players/minimax_player.py
+++ models/players/minimax_player.py
@@ -1,6 +1,8 @@
 ﻿from models.move import Move
 from models.board import Board
 
+
+# Minimax with alpha-beta pruning
 class MinimaxPlayer:
   def __init__(self, color):
     self.color = color
@@ -16,13 +18,13 @@ class MinimaxPlayer:
     for move in board.valid_moves(self.color):
       new_board = board.get_clone()
       new_board.play(move, self.color)
-      curr_value = self.value(new_board, self.color, 0)
+      curr_value = self.value(new_board, self.color, 0, float("-inf"), float("inf"))
       if curr_value > bestValue:
         bestValue = curr_value
         bestMove = move
     return bestMove
 
-  def value(self, board, color, depth):
+  def value(self, board, color, depth, alpha, beta):
     retval = 0
     curr_score = board.score()
     if (curr_score[0] + curr_score[1] == 64) or (depth == self.max_depth):
@@ -32,31 +34,35 @@ class MinimaxPlayer:
         retval = curr_score[1] - curr_score[0]
     else:
       if color == self.color:
-        retval = self.max_value(board, depth)
+        retval = self.max_value(board, depth, alpha, beta)
       else:
-        retval = self.min_value(board, depth)
+        retval = self.min_value(board, depth, alpha, beta)
     return retval
     
-  def max_value(self, board, depth):
+  def max_value(self, board, depth, alpha, beta):
     v = float('-inf')
     if board.valid_moves(self.color).__len__() > 0:
       for move in board.valid_moves(self.color):
         new_board = board.get_clone()
         new_board.play(move, self.color)
-        v = max(v, self.value(new_board, self.oponents_color(), depth+1))
+        v = max(v, self.value(new_board, self.oponents_color(), depth+1, alpha, beta))
+        if v >= beta: return v
+        alpha = max(alpha, v)
     else:
-      v = self.value(board.get_clone(), self.oponents_color(), depth+1)
+      v = self.value(board.get_clone(), self.oponents_color(), depth+1, alpha, beta)
     return v
     
-  def min_value(self, board, depth):
+  def min_value(self, board, depth, alpha, beta):
     v = float('inf')
     if board.valid_moves(self.color).__len__() > 0:
       for move in board.valid_moves(self.oponents_color()):
         new_board = board.get_clone()
         new_board.play(move, self.oponents_color())
-        v = min(v, self.value(new_board, self.color, depth+1))
+        v = min(v, self.value(new_board, self.color, depth+1, alpha, beta))
+        if v <= alpha: return v
+        beta = min(beta, v)
     else:
-      v = self.value(board.get_clone(), self.color, depth+1)
+      v = self.value(board.get_clone(), self.color, depth+1, alpha, beta)
     return v
 
   def oponents_color(self):
